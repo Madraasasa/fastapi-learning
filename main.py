3a2c42352297beb5fd218fa5333ac83dc4b9ae86
Maruf <36636180+Madraasasa@users.noreply.github.com>
2020-02-12 17:00:08 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,104 @@
+from typing import List
+from fastapi import Depends, FastAPI, HTTPException, File, UploadFile
+from sqlalchemy.orm import Session
+from starlette.requests import Request
+from starlette.responses import Response
+from starlette.responses import HTMLResponse
+import os
+from . import crud, schemas, models
+from .database import SessionLocal, engine
+# from starlette.staticfiles import StaticFiles
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# app.mount("/static", StaticFiles(directory="D://pycharm//fast//static"), name="static")
+
+
+@app.middleware("http")
+async def db_session_middleware(request: Request, call_next):
+    response = Response("Internal server error", status_code=500)
+    try:
+        request.state.db = SessionLocal()
+        response = await call_next(request)
+    finally:
+        request.state.db.close()
+    return response
+
+
+# Dependency
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@app.get("/users/", response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@app.post("/users/{user_id}/items/", response_model=schemas.Item)
+def create_item_for_user(
+    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
+):
+    return crud.create_user_item(db=db, item=item, user_id=user_id)
+
+
+@app.get("/items/", response_model=List[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
+
+
+@app.post("/files/", response_model=schemas.Photo)
+async def create_files(db: Session = Depends(get_db), file: UploadFile = File(...)):
+    # a = {'url': file.filename, 'photo': file.read()}
+    with open('D://pycharm//fast//12.png', 'wb') as f:
+        f.write(file.file.read())
+
+    aaa = await file.read()
+    # aaa = open()
+    return crud.create_photo(db=db, url=file.filename, photo_con=aaa)
+
+
+@app.post("/uploadfiles/")
+async def create_upload_files(files: List[UploadFile] = File(...)):
+    return {"filenames": [file.filename for file in files]}
+
+
+@app.get("/")
+async def main():
+    content = """
+<body>
+<form action="/files/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
